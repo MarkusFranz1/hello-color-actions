3800029787b0f3d789e8e50a2718b7c4d3ad6d33
MarkusFranz1 <mfranz@lamigraf.com>
2024-08-02 14:33:46 +0200

    Hello World from Germany

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
 const hello = () => "Hello World from Germany";
-console.log(hello(Hello World from Germany));
-exports.hello = "hello World from Germany"
+console.log(hello());
+exports.hello = hello;
