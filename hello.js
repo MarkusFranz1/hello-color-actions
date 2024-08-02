750461c368b5b8541d9414b5ebfae5f6df3f4211
MarkusFranz1 <mfranz@lamigraf.com>
2024-08-02 14:08:21 +0200

    Update: Changed Hello from Germany message to 'Hello World from Germany'

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
 const hello = () => "Hello World from Germany";
 console.log(hello(Hello World from Germany));
 exports.hello = "hello World from Germany"
+Update: Changed Hello from Germany message to 'Hello World from Germany'
